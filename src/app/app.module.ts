de7d8712691abedaabf2880ca7356103578ff5a8
lijiang <xingkongdadao@gmail.com>
2018-11-30 00:31:10 +0700

    路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentComponent } from './content/content.component';
-import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
-import { StarsComponent } from './stars/stars.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {MenuComponent} from './menu/menu.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {FooterComponent} from './footer/footer.component';
+import {ContentComponent} from './content/content.component';
+import {StockManageComponent} from './stock/stock-manage/stock-manage.component';
+import {StarsComponent} from './stars/stars.component';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+const routeConfig: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'stock', component: StockManageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -20,11 +28,14 @@ import { StarsComponent } from './stars/stars.component';
     ContentComponent,
     StockManageComponent,
     StarsComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
