cb4ecd2c1422aa449fb1bd2f05b2720f15ee0504
lijiang <xingkongdadao@gmail.com>
2018-11-30 06:45:07 +0700

    菜单循环出，并当前菜单显示激活状态

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,16 +7,31 @@ import {Router} from '@angular/router';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  menus: Array<Menu>;
+  currentMenuId: number;
 
   constructor(public router: Router) {
 
   }
 
   ngOnInit() {
+    this.menus = [
+      new Menu(1, '首页', 'dashboard'),
+      new Menu(2, '股票', 'stock'),
+    ];
   }
 
-  nav(url: string) {
-    this.router.navigateByUrl(url);
+  nav(menu: Menu) {
+    this.router.navigateByUrl(menu.link);
+    this.currentMenuId = menu.id;
   }
 
+}
+
+export class Menu {
+  constructor(public id: number,
+              public name: string,
+              public link: string) {
+
+  }
 }
