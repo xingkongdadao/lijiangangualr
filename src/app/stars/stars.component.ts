5ba8bfdc81eec5ac1ebdaf7fa52a72284cb656fc
lijiang <xingkongdadao@gmail.com>
2018-11-29 21:47:23 +0700

    股票组件，星级组件

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  rating = 0;
+  stars: boolean[];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating);
+    }
+
+    // this.stars = [false, false, false, true, true];
   }
 
 }
