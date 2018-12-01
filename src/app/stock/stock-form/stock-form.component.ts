c3b155d9a59af737c27948b7bd74544f4ad32ff9
lijiang <xingkongdadao@gmail.com>
2018-12-01 13:25:46 +0700

    完成表单绑定

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Stock, StockService} from '../stock.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {toNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_version';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-stock-form',
@@ -11,6 +11,8 @@ import {toNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_versio
 export class StockFormComponent implements OnInit {
   stock: Stock;
   stockId: number;
+  formModel: FormGroup;
+  categories = ['IT', '互联网', '金融'];
 
   constructor(private routeInfo: ActivatedRoute,
               private stockService: StockService,
@@ -19,7 +21,6 @@ export class StockFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    const defaultValue = 0;
     this.stockId = this.routeInfo.snapshot.params['id'];
     this.stockId = Number(this.stockId);
     console.log(this.stockId);
@@ -27,6 +28,23 @@ export class StockFormComponent implements OnInit {
     this.stock = this.stockService.getStockById(this.stockId);
     console.log(this.stock);
 
+    // 创建表单数据
+    const fb = new FormBuilder();
+    this.formModel = fb.group(
+      {
+        name: [this.stock.name, [Validators.required, Validators.minLength(3)]],
+        price: [this.stock.price, [Validators.required]],
+        desc: [this.stock.desc],
+        categories: fb.array([
+          new FormControl(this.stock.categories.indexOf(this.categories[0]) !== -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[1]) !== -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[2]) !== -1),
+
+        ])
+
+
+      }
+    );
   }
 
   cancel() {
@@ -34,7 +52,17 @@ export class StockFormComponent implements OnInit {
   }
 
   save() {
-    console.log(this.stock.rating);
-    this.router.navigateByUrl('/stock');
+    const chineseCategories = [];
+    let index = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.formModel.value.categories[i]) {
+        chineseCategories[index++] = this.categories[i];
+      }
+    }
+    this.formModel.value.categories = chineseCategories;
+    this.formModel.value.rating = this.stock.rating;
+    console.log(this.formModel.value);
+    // console.log(this.stock.rating);
+    // this.router.navigateByUrl('/stock');
   }
 }
