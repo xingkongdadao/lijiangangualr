f74e2ac0a3cd8ab3838d45af5c8a777035a22b0c
lijiang <xingkongdadao@gmail.com>
2018-12-03 16:38:36 +0700

    添加翻转动画测试

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {flipStae_animation} from '../animations/flipState.anim';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  animations: [
+    flipStae_animation
+  ]
 })
 export class DashboardComponent implements OnInit {
-
+  flip = 'inactive';
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleFlip() {
+    this.flip = (this.flip === 'inactive') ? 'active' : 'inactive';
+  }
 }
